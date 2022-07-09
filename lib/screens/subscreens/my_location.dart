29584f3710190f029583c0083a6c76a34053a475
오명훈 <omh02033@gmail.com>
2022-07-09 21:41:24 +0900

    feat: sliding up panel

--- lib/screens/subscreens/my_location.dart
+++ lib/screens/subscreens/my_location.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutterlayout/screens/widgets/sub_top.dart';
+import 'package:flutterlayout/themes/color_theme.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class MyLocationPage extends StatelessWidget {
   const MyLocationPage({Key? key}) : super(key: key);
@@ -9,13 +9,61 @@ class MyLocationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
+      body: SlidingUpPanel(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        collapsed: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('나의 위치는 현재 ', style: TextStyle(
+              color: Color(0xff000000),
+              fontWeight: FontWeight.w700,
+              fontSize: 14
+            ),),
+            Text('3학년 1반에 ', style: TextStyle(
+              color: magenta,
+              fontWeight: FontWeight.w700,
+              fontSize: 14
+            ),),
+            Text('입니다', style: TextStyle(
+              color: Color(0xff000000),
+              fontWeight: FontWeight.w700,
+              fontSize: 14
+            ),),
+          ],
+        ),
+        panel: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SubTop(),
+              Text('나의 위치는 현재 ', style: TextStyle(
+                color: Color(0xff000000),
+                fontWeight: FontWeight.w700,
+                fontSize: 14
+              ),),
+              Text('3학년 1반에 ', style: TextStyle(
+                color: magenta,
+                fontWeight: FontWeight.w700,
+                fontSize: 14
+              ),),
+              Text('입니다', style: TextStyle(
+                color: Color(0xff000000),
+                fontWeight: FontWeight.w700,
+                fontSize: 14
+              ),),
             ],
+          ),
+        ),
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SubTop(),
+              ],
+            ),
           ),
         ),
       ),
